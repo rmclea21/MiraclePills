0b7a1fb389cbc701009be91fc356b25bfbedd718
rmclea21 <robertmclean@mevysens-mbp.attlocal.net>
2016-11-23 12:40:23 -0500

    Revert "Added State Picker"
    This reverts commit 36109108bda9625aa005f081e046528bd5fa5cb2.

--- MiraclePills/ViewController.swift
+++ MiraclePills/ViewController.swift
@@ -8,17 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
-    
-    @IBOutlet weak var statePicker: UIPickerView!
-    @IBOutlet weak var statePickerButton: UIButton!
-    
-    let states = ["Alaska", "Arkansas", "Alabama", "South Carolina", "California"]
+class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.statePicker.dataSource = self
-        statePicker.delegate = self
+        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
@@ -26,28 +20,6 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         // Dispose of any resources that can be recreated.
     }
 
-    @IBAction func stateButtonPressed(_ sender: Any) {
-        
-        statePicker.isHidden = false
-        
-    }
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
-        }
-    
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return states.count
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return states[row]
-    }
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        statePickerButton.setTitle(states[row], for: UIControlState.normal)
-        statePicker.isHidden = true
-    }
-    
-    
-}
 
+}
 
